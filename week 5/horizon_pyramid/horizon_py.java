51708740d55ed0089dcfa50570ec754b03525b12
Ivan Zheng <zivan7644@gmail.com>
2022-10-03 11:31:29 -0400

    horizontal pyramid start

---
+++ week 5/horizon_pyramid/horizon_py.java
@@ -0,0 +1,10 @@
+public class horizon_py {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int y;
+        for(y=0; y<x; y++) {
+            System.out.print('*');
+        }
+    }
+
+}
