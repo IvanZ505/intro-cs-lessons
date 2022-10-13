21537e648bbf27412596f97a97d3217f2bc86a01
Ivan Zheng <zivan7644@gmail.com>
2022-10-13 14:32:15 -0400

    bug fix

--- week 6/classroom/Classroom.java
+++ week 6/classroom/Classroom.java
@@ -1,11 +1,13 @@
+import java.util.Arrays;
+
 public class Classroom {
     public static void main(String[] args) {
-        System.out.print("How many students in this class? ");
+        // System.out.print("How many students in this class? ");
         int nbrStudents = StdIn.readInt();
 
         String[] inLine = new String[nbrStudents];
         for(int i = 0; i < inLine.length; i++) {
-            System.out.printf("Enter Student %d's name: ",i);
+            // System.out.printf("Enter Student %d's name: ",i);
             inLine[i] = StdIn.readString();
         }
         System.out.println();
@@ -13,9 +15,9 @@ public class Classroom {
             System.out.print(stu + " - ");
         }
 
-        System.out.print("# rows: ");
+        // System.out.print("# rows: ");
         int rows = StdIn.readInt();
-        System.out.print("# cols: ");
+        // System.out.print("# cols: ");
         int col = StdIn.readInt();
 
         String[][] room = new String[rows][col];
@@ -23,11 +25,19 @@ public class Classroom {
         int i = 0;
         for(int x = 0; x<room.length; x++) {
             for(int y = 0; y<room[x].length; y++) {
-                room[x][y] = inLine[i];
-                i++;
+                try {
+                    room[x][y] = inLine[i];
+                    i++;
+                } catch (IndexOutOfBoundsException e) {
+                    room[x][y] = "empty";
+            }
             }
 
-        }
 
+        }
+        System.out.println();
+        for (String[] strings : room) {
+            System.out.println(Arrays.toString(strings));
+        }
     }
 }
